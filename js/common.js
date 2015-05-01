3d018fcc42fd81344a3cfb877c8382ace3aacde8
Veremey <Veremey@i.ua>
2015-05-01 17:02:04 +0300

    с бекера

--- js/common.js
+++ js/common.js
@@ -1625,6 +1625,38 @@ function init () {
     }
 
 
+// слайдер проворачивающий экземпляры сайтов
+
+
+    function init_cycle() {
+        if ($(".js-slider").length > 0) {
+
+            $(".js-slider").each(function(){
+                var slider_1 = $(this).find('.js-cycle-1');
+                var slider_2 = $(this).find('.js-cycle-2');
+                var prev_nav = $(this).find('.js-cycle-prev');
+                var next_nav = $(this).find('.js-cycle-next');
+                slider_1.cycle({
+                    prev: prev_nav,
+                    next: next_nav
+                });
+                slider_2.cycle();
+
+                var slideshows = $(this).find('.js-slider-cycle').on('cycle-next cycle-prev', function(e, opts) {
+                    // advance the other slideshow
+                    slideshows.not(this).cycle('goto', opts.currSlide);
+                });
+
+                slider_2.find(".cycle-slide").click(function(){
+                    var index = slider_2.data('cycle.API').getSlideIndex(this);
+                    slideshows.cycle('goto', index);
+                });
+            });
+        }
+    }
+    init_cycle();
+
+
 
 }
 	console.log($('body').html());
