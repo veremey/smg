8748f2844edb43b55cd852b354cd7614beded667
Veremey <Veremey@i.ua>
2015-06-29 14:01:24 +0300

    fix

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 head.load("js/lib/jquery.js",
 					"js/lib/slick.min.js",
 					"js/lib/cycle.js",
-					"js/yandexMapInit.js",
+					// "js/yandexMapInit.js",
 					"js/lib/jquery.validate.js",
 					"js/picturefill.js",
 					"js/common.js");
